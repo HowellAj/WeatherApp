3d128c522bb65545f35a36961170d8b759f061b1
W0439218 <w0439219@campus.nscc.ca>
2025-09-15 16:18:17 -0300

    Project Setup

--- app/src/main/java/com/example/weatherapp/MainActivity.kt
+++ app/src/main/java/com/example/weatherapp/MainActivity.kt
@@ -4,13 +4,18 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.weatherapp.ui.theme.WeatherAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +24,56 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             WeatherAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                Home()
+                //Hello()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
+fun Home() {
+
+    Row(horizontalArrangement = Arrangement.Start){
+        Text(
+            text = "Home!",
+            color = Color.Blue,
+            fontSize = 24.sp,
+            modifier = Modifier.padding(32.dp)
+
+        )
+
+        Text(
+            text = "Home 2!",
+            color = Color.Blue,
+            fontSize = 8.sp,
+            modifier = Modifier.padding(32.dp)
+
+        )
+
+        Text(
+            text = "Home 3!",
+            color = Color.Blue,
+            fontSize = 24.sp,
+            modifier = Modifier.padding(32.dp)
+
+        )
+    }
+
+
+
+
+}
+
+@Composable
+fun Hello() {
+    //Currently not in use
     Text(
-        text = "Hello $name!",
-        modifier = modifier
+        text = "Hello!",
+        color = Color.Red,
+        fontSize = 64.sp,
+        modifier = Modifier.padding(32.dp)
+
     )
 }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    WeatherAppTheme {
-        Greeting("Android")
-    }
-}
