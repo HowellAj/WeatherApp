10a29182d11a1be6b1f7d7210d29ee404d3bb9e6
HowellAj <W0439218@campus.nscc.ca>
2025-10-02 09:36:32 -0300

    NavBar Added

--- app/src/main/java/com/example/weatherapp/MainActivity.kt
+++ app/src/main/java/com/example/weatherapp/MainActivity.kt
@@ -8,14 +8,29 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.example.weatherapp.ui.screens.CurrentWeather
 import com.example.weatherapp.ui.screens.DailyForecast
 import com.example.weatherapp.ui.theme.WeatherAppTheme
@@ -26,18 +41,107 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             WeatherAppTheme {
-                //These are the two screens to develop, hardcode which appears for now by commenting one out
-                //Current weather will act as a "home page" or landing page
-                //CurrentWeather()
-
-
-                DailyForecast()
+                DisplayUI();
             }
         }
     }
 }
 
 
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun DisplayUI() {
 
+    val navController = rememberNavController()
+
+    // Store the selectedIndex in a state flow variable
+    var selectedIndex by remember { mutableIntStateOf(0) }
+
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primaryContainer,
+                    titleContentColor = MaterialTheme.colorScheme.primary
+                ),
+                title = {
+                    Text("Local Weather App")
+                }
+            )
+        },
+        bottomBar = {
+            // Display a navigation bar
+            NavigationBar (
+                containerColor = MaterialTheme.colorScheme.primaryContainer,
+                contentColor = MaterialTheme.colorScheme.primary
+            )
+            {
+                // Navigation Bar Items go here...
+
+                // Current weather screen
+                NavigationBarItem(
+                    icon = {
+                        Icon(
+                            painter = painterResource(R.drawable.ic_action_currentweather),
+                            contentDescription = "Current"
+                        )
+                    },
+                    label = {
+                        Text("Current")
+                    },
+                    selected = selectedIndex == 0,
+                    onClick = {
+                        selectedIndex = 0
+                        navController.navigate(route = "CurrentWeather")
+                    }
+                )
+
+                // Forecast weather screen
+                NavigationBarItem(
+                    icon = {
+                        Icon(
+                            painter = painterResource(R.drawable.ic_action_forecast),
+                            contentDescription = "Forecast"
+                        )
+                    },
+                    label = {
+                        Text("Forecast")
+                    },
+                    selected = selectedIndex == 1,
+                    onClick = {
+                        selectedIndex = 1
+                        navController.navigate(route = "DailyForecast")
+                    }
+                )
+            }
+        }
+
+    ) {
+        innerPadding ->
+        // Display screens inside a NavHost()
+
+        NavHost(
+            navController = navController,
+            startDestination = "currentweather",
+            modifier = Modifier.padding(innerPadding)
+        )
+        {
+            // Render Current weather screen
+            composable(route = "currentweather") {
+                CurrentWeather()
+            }
+
+            // Render Forecast screen
+            composable(route = "dailyforecast") {
+                DailyForecast()
+            }
+        }
+
+    }
+
+
+
+
+}
 
 
