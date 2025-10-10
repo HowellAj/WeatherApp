d5f0b18148820291350f6ad6e67f941321d6f31a
HowellAj <W0439218@campus.nscc.ca>
2025-10-10 11:32:38 -0300

    Data models created w/ premade data, need to make data display

--- app/src/main/java/com/example/weatherapp/MainViewModel.kt
+++ app/src/main/java/com/example/weatherapp/MainViewModel.kt
@@ -1,5 +1,71 @@
 package com.example.weatherapp
 import androidx.lifecycle.ViewModel
+import com.example.weatherapp.models.CurrentWeather
+import com.example.weatherapp.models.ForecastWeather
+import com.example.weatherapp.models.WeatherPack
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
 
-class MainViewModel : ViewModel() {
+class MainViewModel : ViewModel()
+{
+    private val _weather = MutableStateFlow(null)
+
+    val weather =_weather.asStateFlow()
+
+    init {
+
+        val weatherData = WeatherPack(
+            CurrentWeather(
+                currentStatus = "Sunny",
+                temperature = "18C",
+                precipitation = "0mm",
+                windSpeed = "10kph",
+                windDirection = "NW",
+                humidity = "12%",
+                date = "10/5/25",
+            ),
+            dailyWeather = listOf(
+                ForecastWeather(
+                    day = 1,
+                    date = "10/6/25",
+                    weatherStatus = "Rain",
+                    temperature = "16C",
+                    precipitation = "15mm",
+                    windSpeed = "15kph",
+                    windDirection = "W",
+                    humidity = "100%"
+                ),
+                ForecastWeather(
+                    day = 2,
+                    date = "10/8/25",
+                    weatherStatus = "Clouds",
+                    temperature = "18C",
+                    precipitation = "0mm",
+                    windSpeed = "8kph",
+                    windDirection = "SW",
+                    humidity = "21%"
+                ),
+                ForecastWeather(
+                    day = 3,
+                    date = "10/10/25",
+                    weatherStatus = "Sun & Cloud",
+                    temperature = "20C",
+                    precipitation = "0mm",
+                    windSpeed = "21kph",
+                    windDirection = "E",
+                    humidity = "12%"
+                ),
+            ),
+
+        )
+
+
+
+
+    }
+
+
+
+
+
 }
--- app/src/main/java/com/example/weatherapp/models/Weather.kt
+++ app/src/main/java/com/example/weatherapp/models/Weather.kt
@@ -1,2 +1,33 @@
 package com.example.weatherapp.models
 
+import android.R.string
+
+
+data class WeatherPack(
+    val currentWeather: CurrentWeather,
+    val dailyWeather: List<ForecastWeather>
+)
+
+data class CurrentWeather(
+    val currentStatus: String,
+    val temperature: String,
+    val precipitation: String,
+    val windSpeed: String,
+    val windDirection: String,
+    val humidity: String,
+    val date: String
+
+        )
+
+data class ForecastWeather(
+    val day: Int,
+    val date: String,
+    val weatherStatus: String,
+    val temperature: String,
+    val precipitation: String,
+    val windSpeed: String,
+    val windDirection: String,
+    val humidity: String
+
+)
+
