4396c56fb40120e93ffff01cc84c035fc6eb7b89
HowellAj <W0439218@campus.nscc.ca>
2025-10-27 17:22:50 -0300

    Location integration started

--- app/src/main/java/com/example/weatherapp/MainActivity.kt
+++ app/src/main/java/com/example/weatherapp/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.weatherapp
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -21,22 +24,31 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.core.content.ContextCompat
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.example.weatherapp.ui.screens.CurrentWeather
 import com.example.weatherapp.ui.screens.DailyForecast
 import com.example.weatherapp.ui.theme.WeatherAppTheme
+import com.google.accompanist.permissions.ExperimentalPermissionsApi
+import com.google.accompanist.permissions.isGranted
+import com.google.accompanist.permissions.rememberPermissionState
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.location.Priority
+import com.google.android.gms.tasks.CancellationTokenSource
 import kotlin.getValue
 
 class MainActivity : ComponentActivity() {
@@ -49,12 +61,73 @@ class MainActivity : ComponentActivity() {
         setContent {
             WeatherAppTheme {
                 mainViewModel.getWeather()
-
+                GetLocation()
                 DisplayUI(mainViewModel);
             }
         }
     }
+
+    @OptIn(ExperimentalPermissionsApi::class)
+    @Composable
+    fun GetLocation() {
+        // Remember the permission state(asking for Fine location)
+        val permissionState = rememberPermissionState(Manifest.permission.ACCESS_FINE_LOCATION)
+
+        if (permissionState.status.isGranted) {
+            Log.i("TESTING", "Hurray, permission granted!")
+
+            // Get Location
+            val currentContext = LocalContext.current
+            val fusedLocationClient = LocationServices.getFusedLocationProviderClient(currentContext)
+
+            if (ContextCompat.checkSelfPermission(
+                    currentContext,
+                    Manifest.permission.ACCESS_FINE_LOCATION
+                ) == PackageManager.PERMISSION_GRANTED)
+            {
+                val cancellationTokenSource = CancellationTokenSource()
+
+                Log.i("TESTING", "Requesting location...")
+
+                fusedLocationClient.getCurrentLocation(Priority.PRIORITY_HIGH_ACCURACY, cancellationTokenSource.token)
+                    .addOnSuccessListener { location ->
+                        if (location != null) {
+                            val lat = location.latitude.toString()
+                            val lng = location.longitude.toString()
+
+                            Log.i("TESTING", "Success: $lat $lng")
+
+                            val coordinates = "$lat,$lng"
+
+                            // call a function, like in View Model, to do something with location...
+                        }
+                        else {
+                            Log.i("TESTING", "Problem encountered: Location returned null")
+                        }
+                    }
+            }
+        }
+        else {
+            // Run a side-effect (coroutine) to get permission. The permission popup.
+            LaunchedEffect(permissionState){
+                permissionState.launchPermissionRequest()
+            }
+        }
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 @OptIn(ExperimentalMaterial3Api::class)
